18d82a60b7f764894575928197b59399e3eccee7
Pedro Fernando <fer321nando@gmail.com>
2022-07-04 23:38:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 
+:root{
+  --squaresize: 50px;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+}
+
+main{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  border-bottom: 7px solid salmon;
+}
+
+.random{
+  display: flex;
+  margin-bottom: -25px;
+  height: var(--squaresize);
+  width: var(--squaresize);
+  justify-content: center;
+  align-items: center;
+}
+
+.game{
+  padding-top: calc(var(--squaresize) * 2);
+  display: flex;
+  justify-content: stretch;
+  gap: 2rem;
+}
+
+.column{
+  flex-basis: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
